9ec1e81a3f8b5e772cd2c31221ec4446bfa6c90e
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2024-08-12 15:09:50 -0400

    add out of sync staff

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,5 +1,5 @@
 use macroquad::{
-    shapes::{draw_arc, draw_circle, draw_line},
+    shapes::{draw_arc, draw_circle, draw_line, draw_rectangle},
     window::clear_background,
 };
 use num_rational::{Ratio, Rational32};
@@ -8,7 +8,7 @@ use num_traits::{FloatConst, ToPrimitive};
 use crate::{
     music::{Part, PianoPhase, Segment},
     timing::Timing,
-    util::remap,
+    util::{lerp, remap},
     visualizer::{
         colors::ColorExt,
         notation::{Font, Staff, StaffPosition},
@@ -45,6 +45,7 @@ impl Visualizer {
         }
 
         draw_in_sync_staff(&self.font, music, current_time);
+        draw_out_of_sync_staff(&self.font, music, current_time, part1_segment_index, part2_segment_index);
 
         true
     }
@@ -121,7 +122,6 @@ fn draw_in_sync_staff(font: &Font, music: &PianoPhase, current_time: f32) {
     top_staff.draw(colors::FOREGROUND_COLOR);
     bottom_staff.draw(colors::FOREGROUND_COLOR);
 
-    // notes
     let draw_past_notes = |staff: &Staff, part: &Part, window_duration: Rational32, stem_end_y: f32| {
         let notes = part.find_note_range(
             |note| note.time.to_f32().unwrap() < (current_time - window_duration.to_f32().unwrap()),
@@ -147,8 +147,8 @@ fn draw_in_sync_staff(font: &Font, music: &PianoPhase, current_time: f32) {
 
             let note_fade = remap(
                 note.time.to_f32().unwrap(),
-                current_time - window_duration.to_f32().unwrap() * 0.9,
-                current_time - window_duration.to_f32().unwrap() * 0.6,
+                current_time - window_duration.to_f32().unwrap() * 0.8,
+                current_time - window_duration.to_f32().unwrap() * 0.5,
                 0.0,
                 1.0,
             );
@@ -174,5 +174,78 @@ fn draw_in_sync_staff(font: &Font, music: &PianoPhase, current_time: f32) {
         let window_length = music.part1.segments[base_time_segment_index].single_measure_duration();
         draw_past_notes(&top_staff, &music.part1, window_length, -3.0);
         draw_past_notes(&bottom_staff, &music.part2, window_length, 8.0);
+    }
+}
+
+fn draw_out_of_sync_staff(
+    font: &Font,
+    music: &PianoPhase,
+    current_time: f32,
+    part1_segment_index: Option<usize>,
+    part2_segment_index: Option<usize>,
+) {
+    // TODO: this code was copied and pasted from draw_in_sync_staff and duplicates a lot of it
+
+    const STAFF_LEFT: f32 = 200.0;
+    const STAFF_1_TOP_Y: f32 = 800.0;
+    const STAFF_2_TOP_Y: f32 = 900.0;
+
+    // TODO: also calculate this
+    // this is measured in staff spaces now
+    const NOTE_HORIZ_SPACE: f32 = 5.0;
+
+    // TODO: calculate these positions instead of hardcoding them
+    let top_staff = Staff::new(font, StaffPosition::Straight { top: STAFF_1_TOP_Y, left: STAFF_LEFT, right: 800.0 }, 10);
+    let bottom_staff = Staff::new(font, StaffPosition::Straight { top: STAFF_2_TOP_Y, left: STAFF_LEFT, right: 800.0 }, 10);
+
+    let go = |segment: &Segment, staff: &Staff, staff_top: f32, stem_end_y: f32| {
+        let offset_in_segment =
+            (current_time - segment.start_time.to_f32().unwrap()) / (segment.end_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap());
+        let current_measure = segment.find_measure(current_time);
+        let offset_in_measure =
+            remap(current_time, current_measure.start_time.to_f32().unwrap(), current_measure.end_time.to_f32().unwrap(), 0.0, 1.0);
+
+        let pattern_len = segment.pattern.0.len();
+
+        let current_dynamic = segment.dynamic.interpolate(offset_in_segment);
+
+        let foreground_color = colors::FOREGROUND_COLOR.modify_a(|a| a * current_dynamic);
+        let highlight_color = colors::HIGHLIGHT_COLOR.modify_a(|a| a * current_dynamic);
+
+        staff.draw(colors::FOREGROUND_COLOR);
+
+        let last_note_x_position = pattern_len as f32 * NOTE_HORIZ_SPACE;
+        for (note_i, note) in segment.pattern.0.iter().enumerate() {
+            let note_x = remap(note_i as f32, 0.0, pattern_len as f32, 0.0, last_note_x_position);
+
+            // only the first and last notes draw beams to simplify things
+            // we can't just draw to a fixed offset because that would draw the beam to a certain position which doesn't account for the stem offset
+            let (beam_left, beam_right) = if note_i == 0 {
+                // draw a beam from the first note to the middle of the staff
+                (None, Some(lerp(0.0, last_note_x_position, 0.5)))
+            } else if note_i == segment.pattern.0.len() - 1 {
+                // draw a beam from the middle of the staff to the last note
+                (Some(lerp(0.0, last_note_x_position, 0.5)), None)
+            } else {
+                (None, None)
+            };
+
+            staff.draw_note(note_x, *note, foreground_color, stem_end_y, 2, beam_left, beam_right)
+        }
+
+        draw_rectangle(
+            STAFF_LEFT,
+            staff_top,
+            lerp(0.0, last_note_x_position, offset_in_measure) * staff.staff_space as f32,
+            staff.staff_height as f32,
+            highlight_color,
+        );
+    };
+
+    if let Some(part1_segment_index) = part1_segment_index {
+        go(&music.part1.segments[part1_segment_index], &top_staff, STAFF_1_TOP_Y, -3.0);
+    }
+    if let Some(part2_segment_index) = part2_segment_index {
+        go(&music.part2.segments[part2_segment_index], &bottom_staff, STAFF_2_TOP_Y, 8.0);
     }
 }
