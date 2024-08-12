21afb73b4e9edcb7e26947383e74cd9feec91be4
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2024-08-11 21:29:18 -0400

    clean up drawing code a bit

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,16 +1,18 @@
 use macroquad::{
     color::{self, Color},
     shapes::{draw_arc, draw_circle, draw_line},
-    text::{draw_text_ex, TextParams},
+    text::draw_text_ex,
     window::clear_background,
 };
+use num_rational::Rational32;
 use num_traits::{FloatConst, ToPrimitive};
-use smufl::{Coord, StaffSpaces};
+use smufl::StaffSpaces;
 
 use crate::{
-    music::{Part, PianoPhase},
+    music::{PianoPhase, Segment},
     timing::Timing,
-    visualizer::notation::Font,
+    util::remap,
+    visualizer::notation::{Font, StaffParams},
 };
 
 mod notation;
@@ -32,193 +34,180 @@ impl Visualizer {
         let part1_segment_index = music.part1.find_current_segment(current_time);
         let part2_segment_index = music.part2.find_current_segment(current_time);
 
-        let draw_wheel = |center_x: f32, center_y: f32, part: &Part, segment_index: Option<usize>| {
-            let Some(segment_index) = segment_index else { return };
-            let segment = &part.segments[segment_index];
-
-            let offset_in_segment = (current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap())
-                / (segment.end_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap());
-            let offset_in_pattern =
-                ((current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap()) / segment.single_pattern_duration().to_f32().unwrap()).fract();
-            let offset_in_pattern_rounded = (offset_in_pattern * segment.pattern.0.len() as f32).floor() / segment.pattern.0.len() as f32;
-
-            let current_dynamic = segment.dynamic.interpolate(offset_in_segment);
-
-            let color = Color::from_rgba(255, 255, 255, (255.0 * current_dynamic) as u8);
-
-            const SPINNER_LENGTH: f32 = 100.0;
-            const ARC_RADIUS: f32 = 150.0;
-
-            let spinner_end_x = center_x + (offset_in_pattern * f32::TAU() - f32::PI() / 2.0).cos() * SPINNER_LENGTH;
-            let spinner_end_y = center_y + (offset_in_pattern * f32::TAU() - f32::PI() / 2.0).sin() * SPINNER_LENGTH;
-            draw_line(center_x, center_y, spinner_end_x, spinner_end_y, 10.0, color);
-
-            let dot_x = center_x + (offset_in_pattern_rounded * f32::TAU() - f32::PI() / 2.0).cos() * ((SPINNER_LENGTH + ARC_RADIUS) / 2.0);
-            let dot_y = center_y + (offset_in_pattern_rounded * f32::TAU() - f32::PI() / 2.0).sin() * ((SPINNER_LENGTH + ARC_RADIUS) / 2.0);
-            draw_circle(dot_x, dot_y, 7.0, color);
-
-            draw_arc(center_x, center_y, 56, ARC_RADIUS, -90.0, 10.0, 360.0 * offset_in_pattern, color);
-        };
-
+        const SPINNER_LENGTH: f32 = 100.0;
+        const ARC_RADIUS: f32 = 150.0;
         // TODO: adjust to window size
-        draw_wheel(1280.0 * 0.25, 720.0 / 2.0, &music.part1, part1_segment_index);
-        draw_wheel(1280.0 * 0.75, 720.0 / 2.0, &music.part2, part2_segment_index);
-
-        {
-            // drawing staff
-
-            const STAFF_SPACE: u32 = 10;
-            const STAFF_HEIGHT: u32 = STAFF_SPACE * 4;
-
-            // TODO: eventually calculate these instead of hardcoding them
-            const STAFF_LEFT: f32 = 200.0;
-            const STAFF_RIGHT: f32 = 800.0;
-            const STAFF_TOP_Y: f32 = 600.0;
-
-            const NOTE_HORIZ_SPACE: f32 = 50.0;
-            const ACCIDENTAL_SHIFT: f32 = 0.3;
-
-            let staff_line_thickness =
-                self.font.metadata.engraving_defaults.staff_line_thickness.unwrap_or(StaffSpaces(1.0 / 8.0)).0 as f32 * STAFF_SPACE as f32;
-            let stem_thickness =
-                self.font.metadata.engraving_defaults.stem_thickness.unwrap_or(StaffSpaces(3.0 / 25.0)).0 as f32 * STAFF_SPACE as f32;
-
-            // staff lines
-            for i in 0..5 {
-                draw_line(
-                    STAFF_LEFT,
-                    STAFF_TOP_Y + i as f32 * STAFF_SPACE as f32,
-                    STAFF_RIGHT,
-                    STAFF_TOP_Y + i as f32 * STAFF_SPACE as f32,
-                    staff_line_thickness,
-                    color::WHITE,
-                );
-            }
-
-            enum Accidental {
-                Natural,
-                Sharp,
-                #[allow(dead_code)]
-                Flat,
-            }
-            let pitch_to_y_position = |note| match note {
-                64 => (4.0, Accidental::Natural),
-                69 => (2.5, Accidental::Natural),
-                66 => (3.5, Accidental::Sharp),
-                71 => (2.0, Accidental::Natural),
-                73 => (1.5, Accidental::Sharp),
-                74 => (1.0, Accidental::Natural),
-                76 => (0.5, Accidental::Natural),
-                _ => unimplemented!("{} not implemented", note),
-            };
-
-            // TODO: make a helper module to deal with these
-            let smufl_coord_to_tuple = |coord: Coord| (coord.x().0 as f32 * STAFF_SPACE as f32, -coord.y().0 as f32 * STAFF_SPACE as f32);
-
-            let draw_note = |x_position: f32, pitch, stem_up, color| {
-                let (y_position, accidental) = pitch_to_y_position(pitch);
-                let notehead_origin = self
-                    .font
-                    .metadata
-                    .anchors
-                    .get(smufl::Glyph::NoteheadBlack)
-                    .and_then(|anchors| anchors.notehead_origin)
-                    .map(smufl_coord_to_tuple)
-                    .unwrap_or((0.0, 0.0));
-
-                draw_text_ex(
-                    &smufl::Glyph::NoteheadBlack.codepoint().to_string(),
-                    x_position - notehead_origin.0,
-                    STAFF_TOP_Y + y_position * STAFF_SPACE as f32 - notehead_origin.1,
-                    TextParams {
-                        font: Some(&self.font.font),
-                        font_size: STAFF_HEIGHT as u16,
-                        font_scale: 1.0,
-                        font_scale_aspect: 1.0,
-                        rotation: 0.0,
-                        color,
-                    },
-                );
-
-                if let Accidental::Sharp | Accidental::Flat = accidental {
-                    draw_text_ex(
-                        &match accidental {
-                            Accidental::Natural => unreachable!(),
-                            Accidental::Sharp => smufl::Glyph::AccidentalSharp,
-                            Accidental::Flat => smufl::Glyph::AccidentalFlat,
-                        }
-                        .codepoint()
-                        .to_string(),
-                        x_position - NOTE_HORIZ_SPACE * ACCIDENTAL_SHIFT,
-                        STAFF_TOP_Y + y_position * STAFF_SPACE as f32,
-                        TextParams {
-                            font: Some(&self.font.font),
-                            font_size: STAFF_HEIGHT as u16,
-                            font_scale: 1.0,
-                            font_scale_aspect: 1.0,
-                            rotation: 0.0,
-                            color,
-                        },
-                    );
-                }
-
-                let stem_origin = if stem_up {
-                    self.font
-                        .metadata
-                        .anchors
-                        .get(smufl::Glyph::NoteheadBlack)
-                        .and_then(|anchors| anchors.stem_up_se)
-                        .map(smufl_coord_to_tuple)
-                        .unwrap_or((0.0, 0.0))
-                } else {
-                    self.font
-                        .metadata
-                        .anchors
-                        .get(smufl::Glyph::NoteheadBlack)
-                        .and_then(|anchors| anchors.stem_down_nw)
-                        .map(smufl_coord_to_tuple)
-                        .unwrap_or((0.0, 0.0))
-                };
-
-                let stem_end_y =
-                    if stem_up { STAFF_TOP_Y - 3.0 * STAFF_SPACE as f32 } else { STAFF_TOP_Y + STAFF_HEIGHT as f32 + 3.0 * STAFF_SPACE as f32 };
-
-                draw_line(
-                    x_position - notehead_origin.0 + stem_origin.0,
-                    STAFF_TOP_Y + y_position * STAFF_SPACE as f32 - notehead_origin.1 + stem_origin.1,
-                    x_position - notehead_origin.0 + stem_origin.0,
-                    stem_end_y,
-                    stem_thickness,
-                    color,
-                );
-            };
-
-            // notes
-            if let Some(part1_segment_index) = part1_segment_index {
-                let segment = &music.part1.segments[part1_segment_index];
-                let current_dynamic = segment.dynamic.interpolate(
-                    (current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap())
-                        / (segment.end_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap()),
-                );
-                for (i, note) in segment.pattern.0.iter().enumerate() {
-                    let notehead_x = STAFF_LEFT + i as f32 * NOTE_HORIZ_SPACE;
-                    draw_note(notehead_x, *note, true, Color::from_rgba(255, 255, 255, (255.0 * current_dynamic) as u8));
-                }
-            }
-
-            if let Some(part2_segment_index) = part2_segment_index {
-                let segment = &music.part2.segments[part2_segment_index];
-                let current_dynamic = segment.dynamic.interpolate(
-                    (current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap())
-                        / (segment.end_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap()),
-                );
-                for (i, note) in segment.pattern.0.iter().enumerate() {
-                    let notehead_x = STAFF_LEFT + i as f32 * NOTE_HORIZ_SPACE;
-                    draw_note(notehead_x, *note, false, Color::from_rgba(255, 255, 255, (255.0 * current_dynamic) as u8));
-                }
-            }
+        if let Some(part1_segment_index) = part1_segment_index {
+            draw_wheel(current_time, &music.part1.segments[part1_segment_index], 1280.0 * 0.25, 720.0 / 2.0, SPINNER_LENGTH, ARC_RADIUS);
         }
+        if let Some(part2_segment_index) = part2_segment_index {
+            draw_wheel(current_time, &music.part2.segments[part2_segment_index], 1280.0 * 0.75, 720.0 / 2.0, SPINNER_LENGTH, ARC_RADIUS);
+        }
+
+        draw_in_sync_staff(music, current_time, &self.font, part1_segment_index, part2_segment_index);
 
         true
     }
+}
+
+fn draw_wheel(current_time: Rational32, segment: &Segment, center_x: f32, center_y: f32, spinner_radius: f32, arc_radius: f32) {
+    let offset_in_segment = (current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap())
+        / (segment.end_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap());
+    let offset_in_pattern =
+        ((current_time.to_f32().unwrap() - segment.start_time.to_f32().unwrap()) / segment.single_pattern_duration().to_f32().unwrap()).fract();
+    let offset_in_pattern_rounded = (offset_in_pattern * segment.pattern.0.len() as f32).floor() / segment.pattern.0.len() as f32;
+
+    let current_dynamic = segment.dynamic.interpolate(offset_in_segment);
+
+    let color = Color::from_rgba(255, 255, 255, (255.0 * current_dynamic) as u8);
+
+    let spinner_end_x = center_x + (offset_in_pattern * f32::TAU() - f32::PI() / 2.0).cos() * spinner_radius;
+    let spinner_end_y = center_y + (offset_in_pattern * f32::TAU() - f32::PI() / 2.0).sin() * spinner_radius;
+    draw_line(center_x, center_y, spinner_end_x, spinner_end_y, 10.0, color);
+
+    let dot_x = center_x + (offset_in_pattern_rounded * f32::TAU() - f32::PI() / 2.0).cos() * ((spinner_radius + arc_radius) / 2.0);
+    let dot_y = center_y + (offset_in_pattern_rounded * f32::TAU() - f32::PI() / 2.0).sin() * ((spinner_radius + arc_radius) / 2.0);
+    draw_circle(dot_x, dot_y, 7.0, color);
+
+    draw_arc(center_x, center_y, 56, arc_radius, -90.0, 10.0, 360.0 * offset_in_pattern, color);
+}
+
+fn draw_in_sync_staff(
+    music: &PianoPhase,
+    current_time: Rational32,
+    font: &Font,
+    part1_segment_index: Option<usize>,
+    part2_segment_index: Option<usize>,
+) {
+    const STAFF_PARAMS: StaffParams = StaffParams::new(10);
+    // TODO: eventually calculate these instead of hardcoding them
+    const STAFF_LEFT: f32 = 200.0;
+    const STAFF_RIGHT: f32 = 800.0;
+    const STAFF_TOP_Y: f32 = 600.0;
+
+    // TODO: also calculate this
+    const NOTE_HORIZ_SPACE: f32 = 50.0;
+
+    draw_staff(font, &STAFF_PARAMS, STAFF_LEFT, STAFF_RIGHT, STAFF_TOP_Y);
+
+    // notes
+    let draw_notes = |segment: &Segment, stem_end_y: f32| {
+        let current_dynamic = segment.dynamic.interpolate(remap(
+            current_time.to_f32().unwrap(),
+            segment.start_time.to_f32().unwrap(),
+            segment.end_time.to_f32().unwrap(),
+            0.0,
+            1.0,
+        ));
+
+        for (note_index, note_pitch) in segment.pattern.0.iter().enumerate() {
+            let note_x = STAFF_LEFT + note_index as f32 * NOTE_HORIZ_SPACE;
+            draw_note(
+                font,
+                &STAFF_PARAMS,
+                STAFF_TOP_Y,
+                note_x,
+                *note_pitch,
+                stem_end_y,
+                Color::from_rgba(255, 255, 255, (255.0 * current_dynamic) as u8),
+            );
+        }
+    };
+    if let Some(part1_segment_index) = part1_segment_index {
+        let segment = &music.part1.segments[part1_segment_index];
+        draw_notes(segment, STAFF_TOP_Y - 3.0 * STAFF_PARAMS.staff_space as f32);
+    }
+
+    if let Some(part2_segment_index) = part2_segment_index {
+        let segment = &music.part2.segments[part2_segment_index];
+        draw_notes(segment, STAFF_TOP_Y + STAFF_PARAMS.staff_height as f32 + 3.0 * STAFF_PARAMS.staff_space as f32);
+    }
+}
+
+fn draw_staff(font: &Font, staff_params: &StaffParams, left: f32, right: f32, top: f32) {
+    let line_thickness =
+        font.metadata.engraving_defaults.staff_line_thickness.unwrap_or(StaffSpaces(1.0 / 8.0)).0 as f32 * staff_params.staff_space as f32;
+    for i in 0..5 {
+        draw_line(
+            left,
+            top + i as f32 * staff_params.staff_space as f32,
+            right,
+            top + i as f32 * staff_params.staff_space as f32,
+            line_thickness,
+            color::WHITE,
+        );
+    }
+}
+fn draw_note(font: &Font, staff_params: &StaffParams, staff_top_y: f32, x_coord: f32, pitch: u8, stem_end_y: f32, color: Color) {
+    let stem_thickness =
+        font.metadata.engraving_defaults.stem_thickness.unwrap_or(StaffSpaces(3.0 / 25.0)).0 as f32 * staff_params.staff_space as f32;
+
+    enum Accidental {
+        Natural,
+        Sharp,
+        #[allow(dead_code)]
+        Flat,
+    }
+    let (y_position_staff_spaces, accidental) = match pitch {
+        64 => (4.0, Accidental::Natural),
+        69 => (2.5, Accidental::Natural),
+        66 => (3.5, Accidental::Sharp),
+        71 => (2.0, Accidental::Natural),
+        73 => (1.5, Accidental::Sharp),
+        74 => (1.0, Accidental::Natural),
+        76 => (0.5, Accidental::Natural),
+        _ => unimplemented!("{} not implemented", pitch),
+    };
+    let y_coord = staff_top_y + y_position_staff_spaces * staff_params.staff_space as f32;
+
+    let notehead_origin = notation::optional_coord_to_tuple(
+        font.metadata.anchors.get(smufl::Glyph::NoteheadBlack).and_then(|anchors| anchors.notehead_origin),
+        staff_params.staff_space as f32,
+    );
+
+    draw_text_ex(
+        &smufl::Glyph::NoteheadBlack.codepoint().to_string(),
+        x_coord - notehead_origin.x,
+        y_coord - notehead_origin.y,
+        font.make_text_params(staff_params, color),
+    );
+
+    if let Accidental::Sharp | Accidental::Flat = accidental {
+        const ACCIDENTAL_SHIFT: StaffSpaces = StaffSpaces(1.5);
+        draw_text_ex(
+            &match accidental {
+                Accidental::Natural => unreachable!(),
+                Accidental::Sharp => smufl::Glyph::AccidentalSharp,
+                Accidental::Flat => smufl::Glyph::AccidentalFlat,
+            }
+            .codepoint()
+            .to_string(),
+            x_coord - ACCIDENTAL_SHIFT.0 as f32 * staff_params.staff_space as f32,
+            y_coord,
+            font.make_text_params(staff_params, color),
+        );
+    }
+
+    let stem_up = stem_end_y < y_coord;
+
+    let stem_origin = if stem_up {
+        notation::optional_coord_to_tuple(
+            font.metadata.anchors.get(smufl::Glyph::NoteheadBlack).and_then(|anchors| anchors.stem_up_se),
+            staff_params.staff_space as f32,
+        )
+    } else {
+        notation::optional_coord_to_tuple(
+            font.metadata.anchors.get(smufl::Glyph::NoteheadBlack).and_then(|anchors| anchors.stem_down_nw),
+            staff_params.staff_space as f32,
+        )
+    };
+
+    draw_line(
+        x_coord - notehead_origin.x + stem_origin.x,
+        y_coord - notehead_origin.y + stem_origin.y,
+        x_coord - notehead_origin.x + stem_origin.x,
+        stem_end_y,
+        stem_thickness,
+        color,
+    );
 }
--- src/visualizer/notation.rs
+++ src/visualizer/notation.rs
@@ -1,11 +1,19 @@
 use std::{fs::File, io::BufReader};
 
-use smufl::Metadata;
+use macroquad::{color::Color, math::Vec2, text::TextParams};
+use smufl::{Coord, Metadata};
 
 #[allow(clippy::manual_non_exhaustive)]
 pub struct Font {
     pub font: macroquad::text::Font,
     pub metadata: smufl::Metadata,
+    _dont_construct: (),
+}
+
+#[allow(clippy::manual_non_exhaustive)]
+pub struct StaffParams {
+    pub staff_space: u16,
+    pub staff_height: u16,
     _dont_construct: (),
 }
 
@@ -17,4 +25,22 @@ impl Font {
         let font = macroquad::text::load_ttf_font("data/bravura/redist/otf/Bravura.otf").await?;
         Ok(Font { font, metadata, _dont_construct: () })
     }
+
+    pub fn make_text_params(&self, staff_params: &StaffParams, color: Color) -> TextParams {
+        TextParams { font: Some(&self.font), font_size: staff_params.staff_height, font_scale: 1.0, font_scale_aspect: 1.0, rotation: 0.0, color }
+    }
+}
+
+impl StaffParams {
+    pub const fn new(staff_space: u16) -> Self {
+        Self { staff_space, staff_height: staff_space * 4, _dont_construct: () }
+    }
+}
+
+pub fn coord_to_tuple(coord: Coord, staff_space: f32) -> Vec2 {
+    Vec2::new(coord.x().0 as f32 * staff_space as f32, -coord.y().0 as f32 * staff_space as f32)
+}
+
+pub fn optional_coord_to_tuple(coord: Option<Coord>, staff_space: f32) -> Vec2 {
+    coord.map(|c| coord_to_tuple(c, staff_space)).unwrap_or(Vec2::new(0.0, 0.0))
 }
